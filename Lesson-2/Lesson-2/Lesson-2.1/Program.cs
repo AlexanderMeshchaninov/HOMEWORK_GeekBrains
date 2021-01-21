dc966501a49cdf05105ba30c3e27048206b9e37f
Alexander <alexander_freeman85@mail.ru>
2021-01-21 23:25:57 +0300

    add Lesson-2

---
+++ Lesson-2/Lesson-2/Lesson-2.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача № 1:
+            //использовал тип sbyte т.к. он, на мой вгляд, более оптимален для памяти в данной задаче
+            Console.WriteLine("Введите минимальную температуру за сутки:");
+            sbyte minTemperature = Convert.ToSByte(Console.ReadLine());
+
+            Console.WriteLine("Введите максимальную температуру за сутки:");
+            sbyte maxTemperature = Convert.ToSByte(Console.ReadLine());
+
+            //сделал явное приведение к типу sbyte т.к. цифра 2 являлась типом int32
+            sbyte result = (sbyte)((minTemperature + maxTemperature) / 2);
+
+            //здесь указан символ градуса цельсия (\u00B0)
+            //результат будет округляться
+            Console.WriteLine($"Cреднесуточная температура: {result} C\u00B0");
+
+            Console.ReadKey();
+        }
+    }
+}
