bfb376271be8390302f45dce43d174069994db80
Alexander <alexander_freeman85@mail.ru>
2021-01-23 20:37:43 +0300

    Lesson-3_branch

---
+++ Lesson-3/Lesson-3.3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Задача 3:
+                //вариант первый
+
+                Console.WriteLine("Введите строку и нажмите ENTER для вывода вашей реверсивной строки");
+                string userStr = Console.ReadLine();
+                char[] charArr = userStr.ToCharArray();
+                //используется метод Reverse
+                Array.Reverse(charArr);
+                string reverseStr = new string(charArr);
+                Console.WriteLine($"Реверсивная строка: {reverseStr}");
+
+                Console.ReadKey();
+            }
+            {
+                //вариант второй
+                string reverseStr = string.Empty;
+                Console.WriteLine("Введите строку и нажмите ENTER для вывода вашей реверсивной строки");
+                string userStr = Console.ReadLine();
+                //обратный цикл
+                for (int i = userStr.Length-1; i >= 0; i--)
+                {
+                    reverseStr += userStr[i];
+                }
+                Console.WriteLine($"Реверсивная строка: {reverseStr}");
+                
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lesson-3/Lesson-3.4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //(*)Задача 4:
+                string [,] gameArr = new string[10, 10];
+                //расположение кораблей
+                gameArr[1, 1] = "X"; gameArr[7, 6] = "X";
+                gameArr[1, 8] = "X"; gameArr[8, 4] = "X";
+                gameArr[2, 8] = "X"; gameArr[9, 4] = "X";
+                gameArr[3, 8] = "X"; gameArr[9, 6] = "X";
+                gameArr[3, 5] = "X"; gameArr[9, 7] = "X";
+                gameArr[3, 6] = "X"; gameArr[9, 8] = "X"; 
+                gameArr[7, 2] = "X"; gameArr[5, 8] = "X";
+                gameArr[6, 4] = "X"; gameArr[4, 1] = "X"; 
+                gameArr[4, 3] = "X"; gameArr[4, 4] = "X";
+                gameArr[7, 1] = "X"; gameArr[4, 2] = "X";
+                for (int i = 0; i < gameArr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < gameArr.GetLength(1); j++)
+                    {
+                        if (gameArr[i, j] != "X")
+                        {
+                            gameArr[i, j] = "O";
+                        }
+
+                        Console.Write("{0,3}", gameArr[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
