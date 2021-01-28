d063799bc3955f0c0b2c8683c3c69c16c940594e
Alexander <alexander_freeman85@mail.ru>
2021-01-28 21:52:12 +0300

    Lesson-4

---
+++ Lesson-4/Lesson-4.1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4._1
+{
+    class Program
+    {
+        static void GetFullName(string firstName, string lastName, string patronymic)
+        {
+            string[] parameters = { lastName, firstName, patronymic };
+            //использую метод Concat для склеивания строк
+                string str = String.Concat(parameters);
+            Console.WriteLine(str);
+        }
+        static string GetFullNameByParams(params string[] values)
+        {
+            string concatString = "";
+            for (int i = 0; i < values.Length; i++)
+            {
+                concatString += values[i];
+            }
+            return concatString;
+        }
+
+        static void Main(string[] args)
+        {
+            {
+                //Задача 1:
+                //способ первый (как понял задание)
+                GetFullName("Александр", "Мещанинов", "Викторович");
+                GetFullName("Федоров", "Максим", "Петрович");
+                GetFullName("Диана", "Иванова", "Александровна");
+                GetFullName("Александр", "Поляков", "Дмитриевич");
+
+                Console.ReadKey();
+            }
+            {
+                //способ второй (с вводом значений через консоль и использованием params)
+                string[] values = new string [3];
+                Console.WriteLine("Введите имя:");
+                values[0] = Console.ReadLine();
+                Console.WriteLine("Введите фамилию:");
+                values[1] = Console.ReadLine();
+                Console.WriteLine("Введите отчество:");
+                values[2] = Console.ReadLine();
+
+                string concatString = GetFullNameByParams(values[1], values[0], values[2]);
+                Console.WriteLine(concatString);
+                
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
---
+++ Lesson-4/Lesson-4.4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4._4
+{
+    class Program
+    {
+        static long FibonachiNumbers(int number)
+        {
+            
+            if (number == 0 || number == 1)
+            {
+                return number;
+            }
+
+            return FibonachiNumbers(number - 1) + FibonachiNumbers(number - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            //(*)Задача 4:
+            {
+                //вариант первый
+                //можно задать число (длинну цикла) и вывести всю последовательность чисел с 0 до указанного значения
+                Console.WriteLine("Задайте длинну последовательности 'чисел Фибоначчи': ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i <= number; i++)
+                {
+                    Console.WriteLine("-" + FibonachiNumbers(i));
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+            {
+                //вариант второй
+                //сразу получить результат: число Фибоначчи
+                Console.WriteLine("Введите число, чтобы вычислить число Фибоначчи: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Число Фибоначчи: " + FibonachiNumbers(number));
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
