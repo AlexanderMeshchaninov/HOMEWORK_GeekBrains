f16ccc65b44bd80913847c20dd3fe5b7985f41cc
Alexander <alexander_freeman85@mail.ru>
2021-01-28 22:16:28 +0300

    Lesson-4 added

---
+++ Lesson-4/Lesson-4.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4._2
+{
+    class Program
+    {
+        static int GetUserStringToSumNumbers(string userAnswer, out int result)
+        {
+            int sum = 0;
+            //тут строка введенная пользователем разделяется по пробелам, а числа попадают в массив для перебора через цикл
+            string [] numbers = userAnswer.Split(new char[] {' '});
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                //элементы массива "парсятся" в интовое значение и суммируются в переменную sum
+                sum += int.Parse(numbers[i].ToString());
+            }
+
+            return result = sum;
+        }
+        static void Main(string[] args)
+        {
+            //Задача 2:
+            {
+                //Ввод чисел через консоль
+                Console.WriteLine("Введите числа через пробелы: \n Далее нажмите ENTER для того, чтобы получить сумму чисел");
+                string userAnswer = Console.ReadLine();
+                //метод принимающий строку и отдающий (out) результат суммирования чисел в консоль
+                GetUserStringToSumNumbers(userAnswer, out int result);
+
+                Console.WriteLine($"Результат сложения чисел: { result }");
+                Console.ReadKey();
+            }
+        }
+    }
+}
